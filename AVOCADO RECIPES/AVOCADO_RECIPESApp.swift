55ee057293fb433297b022b64a393f2b597d0860
Super Production <yokten10@gmail.com>
2021-11-26 21:32:34 +0700

    Initial Commit

---
+++ AVOCADO RECIPES/AVOCADO_RECIPESApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVOCADO_RECIPESApp.swift
+//  AVOCADO RECIPES
+//
+//  Created by Yok on 26/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct AVOCADO_RECIPESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
